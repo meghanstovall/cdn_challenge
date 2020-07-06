4a67bdc635e79d715e41eccd46207ac7c45d62c3
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 17:29:15 -0600

    Create users location in a database

--- app/controllers/user_locations_controller.rb
+++ app/controllers/user_locations_controller.rb
@@ -1,9 +1,19 @@
 class UserLocationsController < ApplicationController
+  def index
+    @locations = UserLocation.all
+  end
+
   def new
   end
 
   def create
-    @user_location = LocationService.location(params[:latitude], params[:longitude])
+    @user_location = LocationFacade.new(params[:latitude], params[:longitude])
+    location = UserLocation.new({ latitude: params[:latitude],
+                                  longitude: params[:longitude],
+                                  city: @user_location.city,
+                                  state: @user_location.state })
+    location.save
 
+    redirect_to "/locations"
   end
 end
