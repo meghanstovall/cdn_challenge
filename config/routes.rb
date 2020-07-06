c79f9bd399ae09c55232ebc92cae4432e2d6333b
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 17:30:04 -0600

    Add user location index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/locations', to: 'user_locations#index';
   get '/locations/new', to: 'user_locations#new';
-  post '/locations', to: 'user_locations#create'
+  post '/locations', to: 'user_locations#create';
 end
