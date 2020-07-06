daa3155e0c8142cf4214c3f1c1778bf507f118e8
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 17:28:56 -0600

    Complete test for a user inputing their location

--- spec/features/locations/locations_spec.rb
+++ spec/features/locations/locations_spec.rb
@@ -4,13 +4,17 @@ RSpec.describe "users location", type: :feature do
   it "can enter the users current geographic coordinates" do
     visit "/locations/new"
 
-    fill_in 'latitude', with: "39.3603"
-    fill_in 'longitude', with: "104.5969"
+    fill_in 'latitude', with: "39.742043"
+    fill_in 'longitude', with: "-104.991531"
 
     click_button "Submit"
 
-    # location = Location.last
-    # expect(location.latitude).to eq("39.3603")
-    # expect(location.latitude).to eq("104.5969")
+    location = UserLocation.last
+
+    expect(current_path).to eq("/locations")
+    expect(location.latitude).to eq("39.742043")
+    expect(location.longitude).to eq("-104.991531")
+    expect(location.city).to eq("Denver")
+    expect(location.state).to eq("CO")
   end
 end
