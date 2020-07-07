ff74e64fe338921e2d2c7762cfd293115e04046a
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 18:22:34 -0600

    Add show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/locations', to: 'user_locations#index';
-  get '/locations/new', to: 'user_locations#new';
-  post '/locations', to: 'user_locations#create';
+  get '/locations/new', to: 'locations#new';
+  post '/locations', to: 'locations#create';
+  get '/locations/:id', to: 'locations#show'
 end
