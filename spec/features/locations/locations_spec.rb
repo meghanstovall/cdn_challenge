09afc74bf96b24f81475594fff472d824e9e6477
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 18:21:32 -0600

    Change where the page should redirect to after entering in location and what ill be seen on that page

--- spec/features/locations/locations_spec.rb
+++ spec/features/locations/locations_spec.rb
@@ -10,10 +10,12 @@ RSpec.describe "users location", type: :feature do
 
     location = Location.last
 
-    expect(current_path).to eq("/locations")
+    expect(current_path).to eq("/locations/#{location.id}")
     expect(location.latitude).to eq("39.742043")
     expect(location.longitude).to eq("-104.991531")
     expect(location.city).to eq("Denver")
     expect(location.state).to eq("CO")
+
+    expect(page).to have_content("Current Location: Denver, CO")
   end
 end
