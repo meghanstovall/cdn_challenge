63e97702e5dde0b67f1debda7051d64fb29e87da
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 20:38:48 -0600

    Add more to user location spec

--- spec/features/locations/locations_spec.rb
+++ spec/features/locations/locations_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "users location", type: :feature do
-  it "can enter the users current geographic coordinates" do
+
+  before :each do
+    Location.delete_all
+  end
+  
+  it "can enter the users geographic coordinates and two edge caches geographic coordinates" do
     visit "/locations/new"
 
-    fill_in 'latlong', with: "39.742043, -104.991531"
+    within '#location' do
+      fill_in 'latlong', with: "39.742043, -104.991531"
+    end
 
     click_button "Submit"
 
@@ -13,9 +20,12 @@ RSpec.describe "users location", type: :feature do
     expect(current_path).to eq("/locations/#{location.id}")
     expect(location.latitude).to eq("39.742043")
     expect(location.longitude).to eq("-104.991531")
-    expect(location.city).to eq("Denver")
-    expect(location.state).to eq("CO")
+    expect(location.country).to eq("US")
+    expect(location.state).to eq("Colorado")
 
-    expect(page).to have_content("Current Location: Denver, CO")
+    expect(page).to have_content("Current Location: Colorado, US")
+
+    click_on "Enter Server Locations"
+    expect(current_path).to eq("/servers/new")
   end
 end
