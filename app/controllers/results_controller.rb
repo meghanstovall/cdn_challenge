cba351b8ae4b95982c755b774168d2bccc84aa68
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 21:08:52 -0600

    Add index action to results controller, that goes to the result model

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,7 +4,7 @@ class ResultsController < ApplicationController
     server1 = Location.find_by(name: "Server 1")
     server2 = Location.find_by(name: "Server 2")
     location = Location.find_by(name: "User's location")
-require "pry"; binding.pry
-    Result.closest_location(location, server1, server2)
+
+    @closest = Result.closest_location(location, server1, server2)
   end
 end
