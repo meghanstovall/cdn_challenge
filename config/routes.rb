251570316ba32171566ebc49373853814aa4cc66
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 20:39:17 -0600

    Add server and result routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/locations/new', to: 'locations#new';
   post '/locations', to: 'locations#create';
-  get '/locations/:id', to: 'locations#show'
+  get '/locations/:id', to: 'locations#show';
+
+  get '/servers/new', to: 'servers#new';
+  post '/servers', to: 'servers#create';
+
+  get '/results', to: 'results#index'
 end
