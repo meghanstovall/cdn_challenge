04e51c013238035b8d53f1138284c3e53864cc6b
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-07 14:28:17 -0600

    Change test to account for deleted files

--- spec/features/locations/locations_spec.rb
+++ spec/features/locations/locations_spec.rb
@@ -5,27 +5,25 @@ RSpec.describe "users location", type: :feature do
   before :each do
     Location.delete_all
   end
-  
+
   it "can enter the users geographic coordinates and two edge caches geographic coordinates" do
     visit "/locations/new"
 
     within '#location' do
       fill_in 'latlong', with: "39.742043, -104.991531"
+      # Pheonix, AZ
+      fill_in 'server1', with: "33.448376, -112.074036"
+      # Des Moines, IA
+      fill_in 'server2', with: "41.619549, -93.598022"
     end
 
     click_button "Submit"
 
     location = Location.last
 
-    expect(current_path).to eq("/locations/#{location.id}")
-    expect(location.latitude).to eq("39.742043")
-    expect(location.longitude).to eq("-104.991531")
-    expect(location.country).to eq("US")
-    expect(location.state).to eq("Colorado")
+    expect(current_path).to eq("/results")
+    expect(Location.all.count).to eq(3)
 
-    expect(page).to have_content("Current Location: Colorado, US")
-
-    click_on "Enter Server Locations"
-    expect(current_path).to eq("/servers/new")
+    expect(page).to have_content("Closest Server: Arizona, US")
   end
 end
