28a69d868a88bcaafb949f00f74d3dbda08fde33
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-06 21:10:46 -0600

    Make to API calls. Return the server with the shortest miles to the users location

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -3,6 +3,12 @@ class LocationService
     self.get_json("/maps/api/geocode/json?latlng=#{latlong}&key=#{ENV['GOOGLE_API_KEY']}")[:results][0][:address_components]
   end
 
+  def self.closest_location(location, server1, server2)
+    distance1 = self.get_json("/maps/api/distancematrix/json?units=imperial&origins=#{location.state},#{location.country}&destinations=#{server1.state},#{server1.country}&key=#{ENV['GOOGLE_API_KEY']}")[:rows][0][:elements][0][:distance][:text][0...-3].to_i
+    distance2 = self.get_json("/maps/api/distancematrix/json?units=imperial&origins=#{location.state},#{location.country}&destinations=#{server2.state},#{server2.country}&key=#{ENV['GOOGLE_API_KEY']}")[:rows][0][:elements][0][:distance][:text][0...-3].to_i
+    if distance1 < distance2 then server1 else server2 end
+  end
+
   private
     def self.get_json(url)
       response = conn.get(url)
