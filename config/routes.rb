43e7b8687fafede10d0e236b7e7f2b2c0e1be671
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-08 14:01:58 -0600

    Change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/locations/new', to: 'locations#new';
   post '/locations', to: 'locations#create';
-  get '/results', to: 'locations#results'
+  get '/results/:user_id/servers/:server1_id/:server2_id', to: 'locations#results'
 end
