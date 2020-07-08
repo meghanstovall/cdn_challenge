a769c66bd3625dee4316e20e899d53856b5f8260
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-08 14:01:50 -0600

    Change redirect_to route after locations created

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,34 +6,28 @@ class LocationsController < ApplicationController
     user_location = LocationFacade.new(params[:latlong])
     location1 = LocationFacade.new(params[:server1])
     location2 = LocationFacade.new(params[:server2])
-    server1 = Location.new({  name: "Server 1",
+    server1 = Location.new({  name: location1.state + ", " + location1.country,
                               latitude: location1.latitude,
-                              longitude: location1.longitude,
-                              country: location1.country,
-                              state: location1.state })
+                              longitude: location1.longitude })
     server1.save!
 
-    server2 = Location.new({  name: "Server 2",
+    server2 = Location.new({  name: location2.state + ", " + location2.country,
                               latitude: location2.latitude,
-                              longitude: location2.longitude,
-                              country: location2.country,
-                              state: location2.state })
+                              longitude: location2.longitude })
     server2.save!
 
-    user_location = Location.new({ name: "User's location",
+    user_location = Location.new({ name: user_location.state + ", " + user_location.country,
                               latitude: user_location.latitude,
-                              longitude: user_location.longitude,
-                              state: user_location.state,
-                              country: user_location.country })
+                              longitude: user_location.longitude })
     user_location.save!
 
-    redirect_to "/results"
+    redirect_to "/results/#{user_location.id}/servers/#{server1.id}/#{server2.id}"
   end
 
   def results
-    server1 = Location.find_by(name: "Server 1")
-    server2 = Location.find_by(name: "Server 2")
-    location = Location.find_by(name: "User's location")
+    server1 = Location.find_by(id: params[:server1_id])
+    server2 = Location.find_by(id: params[:server2_id])
+    location = Location.find_by(id: params[:user_id])
 
     @results = Result.new(location, server1, server2)
   end
