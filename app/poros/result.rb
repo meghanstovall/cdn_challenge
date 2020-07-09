cb9287f12b2d830a874f9653b3e9b647019df387
Meghan Stovall <meghanstovall15@augustana.edu>
2020-07-09 11:15:05 -0600

    Add rtt and bandwitdh calculation methods to the result PORO

--- app/poros/result.rb
+++ app/poros/result.rb
@@ -1,5 +1,7 @@
 class Result
 
+  # result PORO where all calculations are made
+
   def initialize(location, server1, server2)
     @location = location
     @server1 = server1
@@ -10,13 +12,23 @@ class Result
     distance1 = LocationService.distance(@location, @server1)
     distance2 = LocationService.distance(@location, @server2)
     if distance1 < distance2
-      @server1.name
+      @server1
     else
-      @server2.name
+      @server2
     end
   end
 
   def round_trip_time
+    server = closest_location
+    distance = LocationService.distance(@location, server)
+    # twice the distance, divided by 2 times the speed of propagation
+    2 * distance / (2 * (10 ** 8)).to_f
+  end
 
+  def maximum_bandwidth
+    # link capacity - 10MBdelay(10000 kbps)
+    # 10MBdelay(10000 kbps) * 10^3bits/sec * rtt * 10^-3 sec
+    rtt = round_trip_time
+    (10000 * (10 ** 3) * rtt * (10 ** -3)).to_f
   end
 end
